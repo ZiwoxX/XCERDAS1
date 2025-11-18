1db78ee1cdd1533dc36eec9b91485641c5111876
najim <ainic8761@example.com>
2025-11-18 19:41:31 +0700

    Mapel

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -14,7 +14,7 @@ const Selasa = () => {
 					<div className="flex justify-center items-center text-sm">10.00-10.15</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">Pendidikan Pancasila</div>
+					<div className="w-[50%] text-base  font-medium">Pend. Pancasila</div>
 					<div className="flex justify-center items-center text-sm">10.55-12.15</div>
 				</div>
 
--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -1,31 +1,33 @@
 const Senin = () => {
     return (
         <>
-        <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Fisika</div>
-					<div className="flex justify-center items-center text-sm">8:00-10:00</div>
-				</div>
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Geografi</div>
-					<div className="flex justify-center items-center text-sm">10:15-12:15</div>
-				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">Biologi</div>
-					<div className="flex justify-center items-center text-sm">13:30-15:30</div>
-				</div>
-				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">B.Indonesia</div>
-					<div className="flex justify-center items-center text-sm">10.30-11.50</div>
-				</div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base  font-medium">Fisika</div>
+                <div className="flex justify-center items-center text-sm">08:00-10.00</div>
+            </div>
+            
+            <div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base  font-medium">Istirahat</div>
+                <div className="flex justify-center items-center text-sm">10.00-10.15</div>
+            </div>
+            
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base  font-medium">Geografi</div>
+                <div className="flex justify-center items-center text-sm">10.15-12.15</div>
+            </div>
+            
+            <div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+                <div className="w-[50%] text-base  font-medium">Istirahat</div>
+                <div className="flex justify-center items-center text-sm">12.15-13.30</div>
+            </div>
 
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">11.50-12.30</div>
-				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">Informatika</div>
-					<div className="flex justify-center items-center text-sm">13.00-14.20</div>
-				</div>
+            {/* PERUBAHAN DISINI: Tambahkan border-b-2 border-white */}
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+                <div className="w-[50%] text-base  font-medium">Biologi</div>
+                <div className="flex justify-center items-center text-sm">13.30-15.30</div>
+            </div>
+
+            {/* PERUBAHAN: Div kosong paling bawah dihapus saja karena sudah tidak perlu */}
         </>
     )
 } 
