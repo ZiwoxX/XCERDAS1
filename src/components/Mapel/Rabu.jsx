17ffc63894a1ad0135520f714858549c27942a76
najim <ainic8761@example.com>
2025-11-18 18:32:52 +0700

    bebeng

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -1,7 +1,7 @@
 const Rabu = () => {
     return (
         <>
-        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+        <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
 					<div className="w-[50%] text-base  font-medium">Ips</div>
 					<div className="flex justify-center items-center text-sm">07.20-08.00</div>
 				</div>
--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -1,7 +1,7 @@
 const Senin = () => {
     return (
         <>
-        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+        <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
 					<div className="w-[50%] text-base  font-medium">Fisika</div>
 					<div className="flex justify-center items-center text-sm">8:00-10:00</div>
 				</div>
